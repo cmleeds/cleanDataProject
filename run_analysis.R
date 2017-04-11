f550da4de302c943629c7cf1f56e169e076125f8
cmleeds <cmleeds1989@hotmail.com>
2017-04-10 22:40:32 -0500

    update progress on project

---
+++ run_analysis.R
@@ -0,0 +1,59 @@
+# find and download data
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+fileName <- "Project.zip"
+download.file(fileUrl,fileName,method="curl")
+
+# extract zipped files
+unzip("Project.zip")
+
+
+# load packages
+pks <- c("data.table","dplyr")
+library(data.table)
+
+
+# activity labels, subject labels, feature labels
+labels <- fread("UCI HAR Dataset/activity_labels.txt") 
+features <- fread("UCI HAR Dataset/features.txt") 
+
+# adjust feature labels before attaching to data
+features <- gsub("-","_",features$V2)
+features <- gsub("\\,","_",features)
+features <- gsub("\\()","",features)
+features <- gsub("\\(","",features)
+features <- gsub("\\)","",features)
+features
+
+# training data import and format
+trainx <- fread("UCI HAR Dataset/train/X_train.txt")
+names(trainx) <- features
+subsetFeatures <- names(trainx)[grepl("mean",names(trainx)) | grepl("std",names(trainx))]
+trainx <- trainx[,!subsetFeatures,with=F]
+
+trainy <- fread("UCI HAR Dataset/train/y_train.txt")
+names(trainy) <- "activity"
+
+trainSubj <- fread("UCI HAR Dataset/train/subject_train.txt")
+names(trainSubj) <- "subject"
+
+
+
+# test data import and format
+testx <- fread("UCI HAR Dataset/test/X_test.txt")
+names(testx) <- features
+subsetFeatures <- names(testx)[grepl("mean",names(testx)) | grepl("std",names(testx))]
+testx <- testx[,!subsetFeatures,with=F]
+
+testy <- fread("UCI HAR Dataset/test/y_test.txt")
+names(testy) <- "activity"
+
+testSubj <- fread("UCI HAR Dataset/test/subject_test.txt")
+names(testSubj) <- "subject"
+
+
+# merge datasets
+trainAll <- dplyr::bind_cols(trainSubj,trainy,trainx)
+testAll <- dplyr::bind_cols(testSubj,testy,testx)
+allDat <- rbind(trainAll,testAll)
+
+names(labels) <- c("activity","activityLabel")
